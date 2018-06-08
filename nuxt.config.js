064ac96691017e61e0c5ba8ac74827db40a6e09d
Cabrera Mauricio Hernan <mauricioh_cabrera@hotmail.com>
2018-06-07 21:20:29 -0300

    Nuevos scripts en el package para corregir error de npm run dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
     base: '/codypetts/'
-  },
+  }
+} : {}
+
+module.exports = {
+  ...routerBase,
+
   modules: [
     'bootstrap-vue/nuxt',
     // Or if you have custom bootstrap CSS...
@@ -19,7 +24,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Practicando maquetado de web la web http://codypetts.com/ con nuxt ' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicons.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicons.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,700|Exo:400,500,600,700,800,900' }
     ]
   },
